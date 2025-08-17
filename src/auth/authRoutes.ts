4cda6a6b006f50864e9c2f9827d8744fbe5d4f9e
Maninder Singh <manny.singh.live@gmail.com>
2025-08-17 00:25:36 -0400

    create new user in working order

--- src/auth/authRoutes.ts
+++ src/auth/authRoutes.ts
@@ -1,22 +1,20 @@
 import express, { Response, Request } from 'express'; 
 const router = express.Router();
-
-import { body, check, validationResult } from 'express-validator'; 
-const bcrypt = require('bcrypt'); 
-
-
 import User from '../models/User';
-import passport, { use } from "passport"; 
+import { check, validationResult } from 'express-validator'; 
+const bcrypt = require('bcrypt'); 
+const jwt = require("jsonwebtoken"); 
+import passport from "passport"; 
 
 const userLoginValidation = [
     check("email").isEmail().withMessage("Please provide a valid email address."),
     check("password").notEmpty().withMessage("Password is required")
-]
+];
 
 // @route   POST auth/login
 // @desc    Login using email&password
 // @access  Public
-router.post("/login", userLoginValidation, async (req:Request, res:Response) => {
+router.post("/login", userLoginValidation, async (req : Request, res : Response) => {
     try {
         const result = validationResult(req); 
         
@@ -42,13 +40,72 @@ router.post("/login", userLoginValidation, async (req:Request, res:Response) => 
         console.log(err); 
         res.status(500).json({ msg : "Server error" });
     }
-})
+}); 
+
+const createUserValidation = [
+   check("name", "name is required")
+    .not()
+    .isEmpty(), 
+   check("email", "Please include a valid email")
+    .isEmail(), 
+   check("password")
+    .isLength({ min : 6, max: 20})
+    .withMessage("Password must be between 6 and 20 characters")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/)
+    .withMessage("Password must contain at least one special character")
+]; 
 
 // @route   POST auth/signup
 // @desc    Create a new account using email&password
 // @access  Public
-router.post("/signup", (req, res) => {
+router.post("/signup", createUserValidation, async (req : Request, res : Response) => {
+    try { 
 
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors : result.array() }); 
+        }
+
+        const { name, email, password } = req.body; 
+
+        let user = await User.findOne({ email }); 
+
+        if (user) {
+            return res.status(400).json({ errors : [{ msg : 'User already exists' }] }); 
+        }
+        
+        user = new User({
+            name, 
+            email, 
+            password
+        })
+        
+        const saltRounds = 10;
+        const hash = await bcrypt.hash(password, saltRounds); 
+        user.password = hash; 
+        await user.save();
+
+        //jsonwebtoken (jwt)
+        const payload = { userId : user._id };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn : "1h" }); 
+
+        return res.status(201).json({ 
+            token, 
+            user: {
+                id: user._id, 
+                name : user.name,
+                email : user.email
+            }    
+        });  
+        
+    } catch(err) {
+        console.log(err); 
+        res.status(500).json({ msg : "Sever error." }); 
+    }
 })
 
 // GOOGLE OAUTH --------------------------------------------------------------------------------------
