c5141b97bfd7cbbd1eebc03e9c5c3c84aec5c8e8
Maninder Singh <manny.singh.live@gmail.com>
2025-08-20 12:03:25 -0400

    encrypt/decryptGCM in working order

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,15 +1,41 @@
 import { randomBytes, createCipheriv, createDecipheriv } from "crypto";
 
+const algorithm = 'aes-256-gcm'; 
 
+//TODO ---- generate encryption key
 const key = Buffer.from(process.env.EXCRYPTION_KEY_HEX!, "hex"); 
 
-export function encryptGCM(plainText : String) {
+export function encryptGCM(plainText : string) {
+    
+    if (!process.env.EXCRYPTION_KEY_HEX) throw new Error("EXCRYPTION_KEY_HEX missing"); 
 
+    if (key.length !== 32) {
+        throw new Error("EXCRYPTION_KEY_HEX must be 64 hex chars (32 bytes)"); 
+    }
+    
     const iv = randomBytes(12); 
-    const cipher = createCipheriv('aes-256-gcm', key, iv); 
+    const cipher = createCipheriv(algorithm, key, iv); 
+    //update utf-8 plain text and returns ciphertext bytes.
+    const encrypted = Buffer.concat([cipher.update(plainText, "utf8"), cipher.final()]);
+    const tag = cipher.getAuthTag(); 
 
+    return Buffer.concat([iv, tag, encrypted]).toString("base64"); 
 }
 
 export function decryptGCM(payloadB64 : String) {
 
+    if (!process.env.EXCRYPTION_KEY_HEX) throw new Error("EXCRYPTION_KEY_HEX missing"); 
+
+    if (key.length !== 32) {
+        throw new Error("EXCRYPTION_KEY_HEX must be 64 hex chars (32 bytes)"); 
+    }
+
+    const buffer = Buffer.from(payloadB64, "base64"); 
+    const iv = buffer.subarray(0,12);
+    const tag = buffer.subarray(12, 28);  
+    const data = buffer.subarray(28);  
+    const decipher = createDecipheriv(algorithm, key, iv); 
+    decipher.setAuthTag(tag); 
+    const dec = Buffer.concat([decipher.update(data), decipher.final()]);    
+    return dec.toString("utf-8"); 
 }
